4094befd67e1fe39feda3b1c135032cf9322b1a6
Jihyun <touchjh01@naver>
2021-05-12 18:48:38 +0900

    study assignment

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float rotationSpeed = 360f;
+
+    CharacterController characterController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        if (direction.sqrMagnitude > 0.01f)
+        {
+            Vector3 forward = Vector3.Slerp(
+                transform.forward,
+                direction,
+                rotationSpeed * Time.deltaTime / Vector3.Angle(transform.forward, direction)
+                );
+            transform.LookAt(transform.position + forward);
+        }
+
+        characterController.Move(direction * moveSpeed * Time.deltaTime);
+
+        //if (Input.GetKey(KeyCode.LeftArrow))
+        //{
+        //    transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+        //}
+        //if (Input.GetKey(KeyCode.RightArrow))
+        //{
+        //    transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+        //}
+        //if (Input.GetKey(KeyCode.UpArrow))
+        //{
+        //    transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        //}
+        //if (Input.GetKey(KeyCode.DownArrow))
+        //{
+        //    transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
+        //}
+    }
+}
